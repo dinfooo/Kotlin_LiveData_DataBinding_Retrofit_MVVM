3ac7216b69d90b554de7220067736c0721fe4344
mahdi dammak <dammak.mahdi@gmail.com>
2021-01-05 21:28:20 +0100

    Use WorkManager to schedule background tasks.

--- app/src/main/java/com/dammak/mahdi/FavouriteApplication.kt
+++ app/src/main/java/com/dammak/mahdi/FavouriteApplication.kt
@@ -1,12 +1,22 @@
 package com.dammak.mahdi
 
+import android.os.Build
 import android.util.Log
 import androidx.multidex.MultiDexApplication
+import androidx.work.*
+import com.dammak.mahdi.work.RefreshDataWorker
 import timber.log.Timber
 import timber.log.Timber.DebugTree
+import java.util.concurrent.TimeUnit
 
 class FavouriteApplication : MultiDexApplication() {
 
+    /**
+     * onCreate is called before the first screen is shown to the user.
+     *
+     * Use it to setup any background tasks, running expensive setup operations in a background
+     * thread to avoid delaying app start.
+     */
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
@@ -14,8 +24,33 @@ class FavouriteApplication : MultiDexApplication() {
         } else {
             Timber.plant(ReleaseTree())
         }
+        setupRecurringWork()
     }
 
+}
+
+/**
+ * Setup WorkManager background job to 'fetch' new network data daily.
+ */
+private fun setupRecurringWork() {
+    val constraints = Constraints.Builder()
+        .setRequiredNetworkType(NetworkType.CONNECTED)
+        .setRequiresCharging(true)
+        .setRequiresBatteryNotLow(true)
+        .apply {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                setRequiresDeviceIdle(true)
+            }
+        }
+        .build()
+    val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+        .setConstraints(constraints)
+        .build()
+    WorkManager.getInstance().enqueueUniquePeriodicWork(
+        RefreshDataWorker.WORK_NAME,
+        ExistingPeriodicWorkPolicy.KEEP,
+        repeatingRequest
+    )
 }
 
 class ReleaseTree : Timber.Tree() {
---
+++ app/src/main/java/com/dammak/mahdi/work/RefreshDataWorker.kt
@@ -0,0 +1,30 @@
+package com.dammak.mahdi.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.dammak.mahdi.database.AppDatabase
+import com.dammak.mahdi.repository.FavouriteRepository
+import retrofit2.HttpException
+import timber.log.Timber
+
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+    companion object {
+        const val WORK_NAME = "com.dammak.mahdi.work.RefreshDataWorker"
+    }
+
+    override suspend fun doWork(): Result {
+        val database = AppDatabase.getInstance(applicationContext)
+        val repository = FavouriteRepository(database)
+
+        try {
+            repository.refreshFavourite()
+            Timber.d("Work request for sync is run")
+        } catch (e: HttpException) {
+            return Result.retry()
+        }
+        return Result.success()
+    }
+}
